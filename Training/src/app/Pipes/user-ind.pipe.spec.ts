b1b6ac9e890c9fc0975ff99733bc3a0ed9df7ee9
pujaingale <101799476+pujaingale@users.noreply.github.com>
2022-03-17 16:34:29 +0530

    Delete Training directory

--- Training/src/app/Pipes/user-ind.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { UserIndPipe } from './user-ind.pipe';
-
-describe('UserIndPipe', () => {
-  it('create an instance', () => {
-    const pipe = new UserIndPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
